3fc1cde81e2db84ab14895e472006bd71739a6f5
MariaSofi <maria.landavazo@potros.itson.edu.mx>
2021-04-12 13:40:40 -0700

    Ya pude usar la imagen

---
+++ app/src/main/java/com/example/proyectofinal/pantalladecarga.kt
@@ -0,0 +1,11 @@
+package com.example.proyectofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pantalladecarga : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalladecarga)
+    }
+}
