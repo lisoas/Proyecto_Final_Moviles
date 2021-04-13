4b6e46c9ba4c121f13f050618d2ad128eae735da
MariaSofi <maria.landavazo@potros.itson.edu.mx>
2021-04-12 18:57:57 -0700

    Avance 2

---
+++ app/src/main/java/com/example/proyectofinal/iniciarSesion2.kt
@@ -0,0 +1,11 @@
+package com.example.proyectofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class iniciarSesion2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iniciar_sesion2)
+    }
+}
