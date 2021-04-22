48f133c28def2851a9e235e4fa6e436c9ae90359
MariaSofi <maria.landavazo@potros.itson.edu.mx>
2021-04-22 06:00:51 -0700

    Segundo avance

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -9,13 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val button:Button = findViewById(R.id.btnIniciarSesion) as Button
-        val button2:Button = findViewById(R.id.btnRegistrarse) as Button
+
         button.setOnClickListener{
             var intent: Intent = Intent(this, iniciarSesion2::class.java)
             startActivity(intent)
     }
-
+        val button2:Button = findViewById(R.id.btnRegistrarse) as Button
         button2.setOnClickListener{
             var intent: Intent = Intent(this, registrarse::class.java)
             startActivity(intent)
---
+++ app/src/main/java/com/example/proyectofinal/MenuAjustes.kt
@@ -0,0 +1,11 @@
+package com.example.proyectofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuAjustes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_ajustes)
+    }
+}
