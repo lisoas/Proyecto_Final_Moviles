0631b17f879e7b7f66cf78f39dabf22e49aa1c75
MariaSofi <maria.landavazo@potros.itson.edu.mx>
2021-05-10 02:10:09 -0700

    Login con firebase, aun le falta

--- app/src/main/java/com/example/proyectofinal/iniciarSesion2.kt
+++ app/src/main/java/com/example/proyectofinal/iniciarSesion2.kt
@@ -4,16 +4,67 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class iniciarSesion2 : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_sesion2)
-        val button: Button = findViewById(R.id.iniciar) as Button
 
-        button.setOnClickListener{
+        auth = Firebase.auth
+
+       /* val button: Button = findViewById(R.id.iniciar) as Button*/
+        val btn_ingresar: Button = findViewById(R.id.iniciar)
+
+        /*button.setOnClickListener{
             var intent: Intent =Intent(this, MenuPrincipal_1::class.java)
             startActivity(intent)
+        }*/
+
+        btn_ingresar.setOnClickListener{
+            valida_ingreso()
         }
+
     }
+
+    private fun valida_ingreso(){
+        val et_correo: EditText = findViewById(R.id.campoNombre)
+        val et_contra: EditText = findViewById(R.id.campoContrasenia)
+
+        var correo: String = et_correo.text.toString()
+        var contra: String = et_contra.text.toString()
+
+        if(!correo.isNullOrBlank() && !contra.isNullOrBlank()){
+            ingresarFirebase(correo, contra)
+        }else{
+            Toast.makeText(this, "Ingresar datos",
+                Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun ingresarFirebase(correo: String, contra: String){
+        auth.signInWithEmailAndPassword(correo, contra)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+
+                    val user = auth.currentUser
+                    val intent:  Intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(baseContext, "Error de autenticación.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/example/proyectofinal/registrarse.kt
+++ app/src/main/java/com/example/proyectofinal/registrarse.kt
@@ -4,18 +4,66 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class registrarse : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrarse)
 
-        val button: Button = findViewById(R.id.iniciar2) as Button
+        auth = Firebase.auth
 
-        button.setOnClickListener{
-            var intent: Intent = Intent(this, MenuPrincipal_1::class.java)
-            startActivity(intent)
+        val btn_ingresar: Button = findViewById(R.id.iniciar2)
+
+        btn_ingresar.setOnClickListener{
+            valida_registro()
         }
+    }
 
+    private fun valida_registro(){
+        val et_correo: EditText = findViewById(R.id.campoCorreo)
+        val et_contra1: EditText = findViewById(R.id.campoContrasenia)
+       /* val et_contra2: EditText = findViewById(R.id.et_contra2_reg)*/
+
+        var correo: String = et_correo.text.toString()
+        var contra1: String = et_contra1.text.toString()
+        /*var contra2: String = et_contra2.text.toString()*/
+
+        if(!correo.isNullOrBlank() && !contra1.isNullOrBlank()){
+
+            registrarFirebase(correo, contra1)
+            Toast.makeText(this, "Se ha registrado correctamente",
+                Toast.LENGTH_SHORT).show()
+
+        }else{
+            Toast.makeText(this, "Ingresar datos",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun registrarFirebase(correo:String, contra1:String){
+        auth.createUserWithEmailAndPassword(correo, contra1)
+
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    //Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    Toast.makeText(baseContext, "${user.email} se ha creado correctamente",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    //Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
     }
 }
